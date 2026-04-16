158c714d8ae39d27106b03d33e0ed7398d509641
Murali Krishna <muralikrishna8309@gmail.com>
2026-04-16 17:06:44 +0530

    added cross site origin

--- server/app.js
+++ server/app.js
@@ -2,15 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const dns = require('dns');
+const dns = require("dns");
 
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const app = express();
 
-app.use(cors());
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(
+  cors({
+    origin: [FRONTEND_URL],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URL);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/dashboard", require("./routes/dashboard"));
@@ -19,4 +33,6 @@ app.use("/api/transfer", require("./routes/transfer"));
 app.use("/api/assignment", require("./routes/assignment"));
 app.use("/api/assets", require("./routes/assets"));
 
-app.listen(5000, () => console.log("Server Running"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Running ${PORT}`));
